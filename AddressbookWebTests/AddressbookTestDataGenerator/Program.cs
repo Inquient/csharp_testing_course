4d30df944951b57761c3be027ed7692427c98f3c
Inquient <makoveyfedorich@gmail.com>
2022-04-02 19:12:46 +0300

    Лекция 6.1 Добавлена генерация текстового файла с тестовыми данными

---
+++ AddressbookWebTests/AddressbookTestDataGenerator/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AddressbookWebTests;
+
+namespace AddressbookTestDataGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int testDataCount = Convert.ToInt32(args[0]);
+            StreamWriter writer = new StreamWriter(args[1]);
+            for (int i = 0; i < testDataCount; i++)
+            {
+                writer.WriteLine($"{TestBase.GenerateRandomString(10)}," +
+                                 $"{TestBase.GenerateRandomString(10)}," +
+                                 $"{TestBase.GenerateRandomString(10)}");
+            }
+            writer.Close();
+        }
+    }
+}
